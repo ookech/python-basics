c07d6aa9a1da932fe556dc4c1331ea04539ef2d7
ookech <billyokech16@gmail.com>
2026-02-03 10:25:51 +0300

    python prove

---
+++ introduction.py
@@ -0,0 +1,8 @@
+#print is a method used to display an output
+
+print("Welcome to Python Programing")
+print("Billy")
+print(100)
+print(200*30)
+
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+#variables are containers used to store values
+
+school = "Emobilis"
+firstname = "Billy"
+lastname =  "Omondii"
+num1 = 37
+num2 = 42
+langugage ="Python"
+
+print(school)
+print(firstname,lastname)
+print(num1 + num2)
+print(langugage,"is a web programing language")
+
+print("the sum of" ,num1 , "and" , num2, "is" , num1 + num2)
