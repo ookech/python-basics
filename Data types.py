bff78400c7540deae2a12c5b41ddb14e21e6fd75
ookech <billyokech16@gmail.com>
2026-02-04 10:05:44 +0300

    third commit

---
+++ Data types.py
@@ -0,0 +1,32 @@
+
+age = 18 #integer
+weight = 62.4 #float
+greeting = "Hii There" #string
+isMammal = True #boolean
+
+#Datastructures --Multiple elements in one variable
+fruits =["Banana", "Mangoes", "Apples"] #List ---is orderd and changable  ----different datatypes
+courses = ["Cybersecurity", "Physics", "Datascience", "MIT" ] #array ---similar datatypes
+cars = ("Ford", "G-Wagon" "Mazda", "Mitsubishi") #tuple --orderd elements and are unchangable
+Countries = {"China" , "India" , "Italy"}   #set ---Unordered and unchangable
+student = {
+    "firstname" : "Billy" ,
+    "course" : "MIT" ,
+    "age" : 18 ,
+    "Nationality" : "Kenyan"
+
+} #Dictionary ---key value pair
+
+print("student is" ,age, "years old")
+print(weight)
+print("Is animal a mammal ?" ,isMammal)
+
+
+      #typecasting ---converting one data type to another
+
+print(float(age))
+print(int(weight))
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,13 @@
+greetings ="Hello there"
+course = "SOFTWARE ENGINEERING"
+
+print(greetings[0])
+
+print(len(greetings))
+
+print(course.lower())
+print(greetings.upper())
+
+#strings concatenation --joining strings
+
+print(greetings + course)
